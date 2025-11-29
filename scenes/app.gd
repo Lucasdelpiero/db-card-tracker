d4546c669178e4a14c450391ce39ab32524d3e07
Lucasdelpiero <lucasdelpiero98@gmail.com>
2025-11-29 03:55:45 -0300

    Mejor estructura y con fuentes

--- scenes/app.gd
+++ scenes/app.gd
@@ -1,16 +1,22 @@
 extends Control
 
-@onready var scroll_container_saga : ScrollContainer = %ScrollContainerSaga
-@onready var scroll_container_cards : ScrollContainer = %ScrollContainerCards
+
+@onready var saga_selection : Control = %SagaSelection
+@onready var saga_cards : Control = %SagaCards
 
 func _ready() -> void:
-	scroll_container_saga.visible = true
-	scroll_container_cards.visible = false
+	saga_selection.visible = true
+	saga_cards.visible = false
 
 func _on_box_button_button_pressed() -> void:
-	scroll_container_saga.visible = false
-	scroll_container_cards.visible = true
+	saga_selection.visible = false
+	saga_cards.visible = true
 
 func _on_card_card_pressed() -> void:
-	scroll_container_saga.visible = true
-	scroll_container_cards.visible = false
+	saga_selection.visible = true
+	saga_cards.visible = false
+
+# Vuelve al menu principal
+func _on_button_back_pressed() -> void:
+	saga_selection.visible = true
+	saga_cards.visible = false
