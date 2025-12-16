e3c4f2447432c0e2b29d68d4c7e09b6204512db5
Lucasdelpiero <lucasdelpiero98@gmail.com>
2025-12-16 10:48:46 -0300

    Boton atras cel funciona bien

--- scenes/app.gd
+++ scenes/app.gd
@@ -5,11 +5,26 @@ extends Control
 @onready var card_details = %CardDetails
 
 func _ready() -> void:
+	get_tree().set_auto_accept_quit(false) # game doesnt close when pressed back
 	saga_selection.visible = true
 	saga_cards.visible = false
 	card_details.visible = false
 	saga_selection.saga_changed.connect(change_saga)
 	card_details.state_changed.connect(save_data)
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_GO_BACK_REQUEST:
+		if card_details.visible:
+			card_details.visible = false
+			saga_cards.visible = true
+			saga_selection.visible = false
+			
+		elif saga_cards.visible:
+				saga_cards.visible = false
+				card_details.visible = false
+				saga_selection.visible = true
+		else:
+			get_tree().quit()
 
 func save_data() -> void:
 	saga_cards.save_data()
